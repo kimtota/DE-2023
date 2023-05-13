0ccfc6c0bf933485f4a1c2dc054ecf549e768334
Kim Sein <continue.deve@gmail.com>
2023-05-13 19:29:14 +0900

    Modifying IMDB + Add UBER

--- HW1/IMDBStudent20180950.java
+++ HW1/IMDBStudent20180950.java
@@ -19,20 +19,14 @@ public class IMDBStudent20180950
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString(), "::");
-			int id;
-			String title;
-			String genre;
+			int id = Integer.parseInt(itr.nextToken().trim());
+			String title = itr.nextToken().trim();
+			String genre = itr.nextToken().trim();
 			
-			while (itr.hasMoreTokens()) {
-				id = Integer.parseInt(itr.nextToken().trim());
-				title = itr.nextToken().trim();
-				genre = itr.nextToken().trim();
-				
-				StringTokenizer itr2 = new StringTokenizer(genre, "|");
-				while (itr2.hasMoreTokens()) {
-					word.set(itr2.nextToken().trim());
-					context.write(word, one);
-				}
+			StringTokenizer itr2 = new StringTokenizer(genre, "|");
+			while (itr2.hasMoreTokens()) {
+				word.set(itr2.nextToken().trim());
+				context.write(word, one);
 			}
 		}
 	}
@@ -40,11 +34,9 @@ public class IMDBStudent20180950
 	public static class IMDBReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 		private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
-		{
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
-			for (IntWritable val : values) 
-			{
+			for (IntWritable val : values) {
 				sum += val.get();
 			}
 			result.set(sum);
@@ -52,8 +44,7 @@ public class IMDBStudent20180950
 		}
 	}
 
-	public static void main(String[] args) throws Exception 
-	{
+	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
 		
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
@@ -68,11 +59,14 @@ public class IMDBStudent20180950
 		job.setMapperClass(IMDBMapper.class);
 		job.setCombinerClass(IMDBReducer.class);
 		job.setReducerClass(IMDBReducer.class);
+		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
+		
 		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
 		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+		
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
