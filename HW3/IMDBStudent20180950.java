ca62987ec2b7bda341c7feffad05e1b58c925857
Kim Sein <continue.deve@gmail.com>
2023-06-05 23:27:04 +0900

    Modifying HW3

--- HW3/IMDBStudent20180950.java
+++ HW3/IMDBStudent20180950.java
@@ -28,37 +28,25 @@ public class IMDBStudent20180950
 		
 		FlatMapFunction<String, String> fmf = new FlatMapFunction<String, String>() {
 			public Iterator<String> call(String s) {
-				Iterator<String> list = Arrays.asList(s.split("::")).iterator();
-				int cnt = 0;
-				while(list.hasNext()){
-					if(cnt != 3) {
-						list.next();
-						list.remove();
-						cnt++;
-					}
-					else {
-						list.next();
-						cnt = 0;
-					}
-				}
-				return list;
+				return Arrays.asList(s.split("::")).iterator();
 			}
 		};
 		JavaRDD<String> words1 = lines.flatMap(fmf);
+		JavaRDD<String> words2 = words1.filter(new Function<String, Boolean>() { public Boolean call(String s) { return s.contains("|") == true; }});
 		
 		FlatMapFunction<String, String> fmf2 = new FlatMapFunction<String, String>() {
 			public Iterator<String> call(String s) {
-				return Arrays.asList(s.split("|")).iterator();
+				return Arrays.asList(s.split("//|")).iterator();
 			}
 		};
-		JavaRDD<String> words2 = words1.flatMap(fmf2);
+		JavaRDD<String> words3 = words2.flatMap(fmf2);
 		
 		PairFunction<String, String, Integer> pf = new PairFunction<String, String, Integer>() {
 			public Tuple2<String, Integer> call(String s) {
 				return new Tuple2(s, 1);
 			}
 		};
-		JavaPairRDD<String, Integer> ones = words2.mapToPair(pf);
+		JavaPairRDD<String, Integer> ones = words3.mapToPair(pf);
 		
 		Function2<Integer, Integer, Integer> f2 = new Function2<Integer, Integer, Integer>() {
 			public Integer call(Integer x, Integer y) {
@@ -69,21 +57,5 @@ public class IMDBStudent20180950
 		
 		counts.saveAsTextFile(args[1]);
 		spark.stop();
-		
-		/*
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-		
-		Job job = new Job(conf, "imdb");
-		job.setJarByClass(IMDBStudent20180950.class);
-		job.setMapperClass(IMDBMapper.class);
-		job.setCombinerClass(IMDBReducer.class);
-		job.setReducerClass(IMDBtReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-		*/
 	}
 }
